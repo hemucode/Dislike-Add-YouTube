db4c209574f12ef79b9cf5044e367cac843ea249
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2022-10-05 12:38:48 +0530

    ok

--- Chrome Extension - not edit/lib/config.js
+++
@@ -1,6 +0,0 @@
-var config = {};
-
-config.welcome = {
-  set lastupdate (val) {app.storage.write("lastupdate", val)},
-  get lastupdate () {return app.storage.read("lastupdate") !== undefined ? app.storage.read("lastupdate") : 0}
-};
